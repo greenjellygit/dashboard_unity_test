4a2f4502c8e56921b6b4a62490284bdcf6c309bb
Krzysztof Antczak <krantczak@pzu.pl>
2016-11-11 20:14:45 +0100

    fast commit

--- kudo_dialog/kudo_dialog.js
+++ kudo_dialog/kudo_dialog.js
@@ -1,5 +1,5 @@
 function test() {
 	//HipChat.chat.appendMessage('test');
-	HipChat.chat.addFileForUploadWithBase64("data:text/plain;base64,c2llbWFuZWN6a28=");
+	HipChat.chat.addFileForUploadWithBase64("data:text/plain;base64,c2llbWFuZWN6a28=", "test.txt");
 	//HipChat.dialog.close();
 }}
