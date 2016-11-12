b309ce7e2e37f3714e93c39903231097fb051333
Krzysztof Antczak <krantczak@pzu.pl>
2016-11-13 00:05:12 +0100

    fast commit

--- kudo_dialog/scripts/app.js
+++ kudo_dialog/scripts/app.js
@@ -1,4 +1,23 @@
 angular.module("kudoAddon", [])
+.run(function($rootScope) {
+	
+	$rootScope.oauthId = parseJwt(findUrlParam("signed_request"));
+	
+	function findUrlParam(name) {
+		var url = window.location;
+		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+		var regexS = "[\\?&]"+name+"=([^&#]*)";
+		var regex = new RegExp( regexS );
+		var results = regex.exec( url );
+		return results == null ? null : results[1];
+	}
+	
+	function parseJwt (token) {
+		var base64Url = token.split('.')[1];
+		var base64 = base64Url.replace('-', '+').replace('_', '/');
+		return JSON.parse(window.atob(base64));
+	}
+})
 .controller("ConfigurationController", function($scope) {
-	$scope.test = "asd asd asd";
+	$scope.addonCreditentials = {login: "", pass: ""}
 });
--- kudo_dialog/scripts/kudo_dialog.js
+++ kudo_dialog/scripts/kudo_dialog.js
@@ -4,17 +4,6 @@ function test() {
 	//HipChat.dialog.close();
 }
 
-function showJWT() {
-	var tokenObj = parseJwt(findUrlParam("signed_request"));
-	document.getElementById("resultBox").innerText = tokenObj.iss;
-}
-
-function parseJwt (token) {
-	var base64Url = token.split('.')[1];
-	var base64 = base64Url.replace('-', '+').replace('_', '/');
-	return JSON.parse(window.atob(base64));
-};
-
 function getToken() {
 	var req = new XMLHttpRequest();
 	req.open("POST", "https://api.hipchat.com/v2/oauth/token", true);
@@ -29,11 +18,3 @@ function getToken() {
 	req.send("grant_type=client_credentials&scope=send_notification");
 }
 
-function findUrlParam(name) {
-    var url = window.location;
-    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    var regexS = "[\\?&]"+name+"=([^&#]*)";
-    var regex = new RegExp( regexS );
-    var results = regex.exec( url );
-    return results == null ? null : results[1];
-}
