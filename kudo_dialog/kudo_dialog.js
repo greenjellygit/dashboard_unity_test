3c6506e7976b4f58766af348ecccb6e309c9b601
Krzysztof Antczak <krantczak@pzu.pl>
2016-11-12 01:21:35 +0100

    fast commit

--- kudo_dialog/kudo_dialog.js
+++ kudo_dialog/kudo_dialog.js
@@ -4,6 +4,11 @@ function test() {
 	//HipChat.dialog.close();
 }
 
+function showJWT() {
+	var token = findUrlParam("signed_request");
+	alert(token);
+}
+
 function getToken() {
 	var req = new XMLHttpRequest();
 	req.open("POST", "https://api.hipchat.com/v2/oauth/token", true);
@@ -16,4 +21,13 @@ function getToken() {
 	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 	req.setRequestHeader('Authorization', 'Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW');
 	req.send("grant_type=client_credentials&scope=send_notification");
+}
+
+function findUrlParam(name) {
+    var url = window.location;
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( url );
+    return results == null ? null : results[1];
 }
